efea95551f7bae42f7d7ee8f6d72ace1e1f8213a
윤채령 <yunchaelyeong@yunchaelyeong-ui-MacBookPro.local>
2023-06-19 22:21:08 +0900

    검색엔진 만들기

--- cities/admin.py
+++ cities/admin.py
@@ -1,3 +1,14 @@
+# cities/admin.py
+
 from django.contrib import admin
+from .models import City
+
 
 # Register your models here.
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = ("name", "state",)
+
+
+admin.site.register(City, CityAdmin)
+
--- cities/views.py
+++ cities/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView
+from .models import City
+from django.db.models import Q
+
 
 # Create your views here.
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+
+class SearchResultsView(ListView):
+    model = Exhibition
+    template_name = 'search_results.html'
+
+    def get_queryset(self):  # new
+        return Exhibition.objects.filter(
+            Q(name__icontains='최근') | Q(date__icontains='2023.07')
+        )
